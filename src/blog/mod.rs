974ab06fcc8a9c342eb35f63127372e1289874af
anbo <anbo225@qq.com>
2023-02-24 17:00:46 +0800

    format code

--- src/blog/mod.rs
+++ src/blog/mod.rs
@@ -1,4 +1,3 @@
-
 //! State is a behavioral design pattern that lets an object alter its behavior when its internal state changes.
 //! It appears as if the object changed its class.
 
@@ -7,76 +6,78 @@
 //! 2. When the draft is done, a review of the post is requested.
 //! 3. When the post is approved, it gets published.
 //! 4. Only published blog posts return content to print, so unapproved posts can’t accidentally be published.
-
+//! 5 Add a reject method that changes the post’s state from PendingReview back to Draft.
+//! 6 Require two calls to approve before the state can be changed to Published.
+//! 7 Allow users to add text content only when a post is in the Draft state. Hint: have the state object responsible for what might change about the content but not responsible for modifying the Post.
 
 /// Examples: using traint object to implement state pattern
 /// State: Draft , PendingReview, Published;
-pub struct Post{
-    state :  Option<Box<dyn State>>,
-    content : String,
+
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+    number_of_approvals: i32,
 }
 
 impl Post {
-    pub fn new() -> Post{
-        Post { 
-            state: Some(Box::new(Draft{})), 
-            content: String::new(), 
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+            number_of_approvals: 0,
         }
     }
 
-    pub fn content(&self) -> &str{
+    pub fn content(&self) -> &str {
         // todo: some bug happens here
         &self.state.as_ref().unwrap().content(self)
     }
 
-    pub fn add_text(&mut self, text: &str){
-        
+    pub fn add_text(&mut self, text: &str) {
         self.content = self.state.as_ref().unwrap().add_text(&self.content, text)
     }
 
-   
     pub fn request_review(&mut self) {
         if let Some(s) = self.state.take() {
             self.state = Some(s.request_review())
         }
     }
 
-    pub fn approve(&mut self){
+    pub fn approve(&mut self) {
         if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
+            self.state = Some(s.approve(self))
         }
     }
 
-    pub fn reject(&mut self){
+    pub fn reject(&mut self) {
         if let Some(s) = self.state.take() {
             self.state = Some(s.reject())
         }
     }
 }
 
-trait State{
+trait State {
     fn content<'a>(&self, post: &'a Post) -> &'a str {
         ""
     }
 
-    fn add_text(&self, original_text: &str, _text_to_add: &str) -> String{
-         original_text.to_string()
+    fn add_text(&self, original_text: &str, _text_to_add: &str) -> String {
+        original_text.to_string()
     }
 
     fn request_review(self: Box<Self>) -> Box<dyn State>;
-    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>, post: &mut Post) -> Box<dyn State>;
     fn reject(self: Box<Self>) -> Box<dyn State>;
-
 }
 
 struct Draft {}
 
 impl State for Draft {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
-         Box::new(PendingReview{})
+        Box::new(PendingReview {})
     }
 
-    fn approve(self: Box<Self>) -> Box<dyn State> {
+    fn approve(self: Box<Self>, post: &mut Post) -> Box<dyn State> {
         self
     }
 
@@ -84,30 +85,30 @@ impl State for Draft {
         self
     }
 
-   
-
-    fn add_text<'a>(&self, original_text: &str, _text_to_add: &str) -> String{
-         format!("{}{}", original_text, _text_to_add)
+    fn add_text<'a>(&self, original_text: &str, _text_to_add: &str) -> String {
+        format!("{}{}", original_text, _text_to_add)
     }
-
-    
-    
 }
 
 struct PendingReview {}
 
 impl State for PendingReview {
-    fn request_review(self : Box<Self>) -> Box<dyn State> {
-         self
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
     }
 
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Published{})
+    fn approve(self: Box<Self>, post: &mut Post) -> Box<dyn State> {
+        if post.number_of_approvals < 1 {
+            post.number_of_approvals = post.number_of_approvals + 1;
+            self
+        } else {
+            Box::new(Published {})
+        }
     }
 
     fn reject(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Draft{})
-    }  
+        Box::new(Draft {})
+    }
 }
 
 struct Published {}
@@ -121,7 +122,7 @@ impl State for Published {
         self
     }
 
-    fn approve(self: Box<Self>) -> Box<dyn State> {
+    fn approve(self: Box<Self>, post: &mut Post) -> Box<dyn State> {
         self
     }
 
@@ -130,13 +131,12 @@ impl State for Published {
     }
 }
 
-
 #[cfg(test)]
-mod tests{
-   
+mod tests {
+
     use super::*;
     #[test]
-    fn test_add(){
+    fn test_add() {
         let mut post = Post::new();
         assert_eq!(post.content(), "");
 
@@ -145,7 +145,7 @@ mod tests{
         assert_eq!(post.content(), "");
 
         post.approve();
-        
+
         post.request_review();
         post.add_text(" World");
         assert_eq!(post.content(), "");
@@ -154,8 +154,8 @@ mod tests{
         post.request_review();
         post.approve();
         post.add_text(" World");
-         post.add_text(" World");
-        assert_eq!(post.content(), "Hello World");
+        post.add_text(" World");
+        assert_eq!(post.content(), "");
 
         post.request_review();
         post.approve();
